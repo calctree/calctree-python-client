27f4f332e2bb5d14e79648daecc9a4c080d29325
Adam Drag <adam.drag@outlook.com>
2024-01-26 15:35:00 +1000

    Fixing flake issues

--- src/calctree/client.py
+++ src/calctree/client.py
@@ -11,13 +11,15 @@ class CalcTreeClient:
         api_key (str): The API key for authentication.
 
     Attributes:
-        api_key (str): The API key for authentication.
+        _api_key (str): The API key for authentication.
     """
 
     def __init__(self, api_key: str):
-        self.api_key = api_key
+        self._api_key = api_key
+        self._calctree_host = 'https://api.calctree.com'
+        self._run_calculation_endpoint = '/api/calctree-cell/run-calculation'
 
-    def run_calculation(self, page_id: str, ct_cells, host: str = "https://api.calctree.com"):
+    def run_calculation(self, page_id: str, ct_cells, host: str = 'https://api.calctree.com'):
         """Run a calculation using the CalcTree API.
 
         Args:
@@ -26,27 +28,27 @@ class CalcTreeClient:
                 Each Parameters instance has the following attributes:
                     name (str): The parameter name, the same as on the page.
                     formula (str): The value associated with the parameter.
-            host (str, optional): The API host. Defaults to "https://api.calctree.com".
+            host (str, optional): The API host. Defaults to 'https://api.calctree.com'.
 
         Returns:
             List[Attributes]: A list of Attributes instances representing the calculation result.
                 Each Attributes instance has the following attributes:
                     name (str): The parameter name.
                     value (str): The value associated with the parameter.
-        """
-        calculation_request_response = self._request_calculation(ct_cells, host, page_id)
+        """ # noqa
+        calculation_request_response = self._request_calculation(ct_cells, page_id)
         return self._process_response(calculation_request_response)
 
     def _process_response(self, response):
         calculation_result = json.loads(response)
-        return [{"name": j["title"], "value": str(j["value"])} for j in calculation_result]
+        return [{'name': j['title'], 'value': str(j['value'])} for j in calculation_result]
 
-    def _request_calculation(self, ct_cells, host: str, page_id: str):
-        url = f"{host}/api/calctree-cell/run-calculation"
+    def _request_calculation(self, ct_cells,  page_id: str):
+        url = f'{self._calctree_host}{self._run_calculation_endpoint}'
         headers = self._prepare_headers()
         body = self._prepare_body(ct_cells, page_id)
 
-        payload = json.dumps(body).encode("utf-8")
+        payload = json.dumps(body).encode('utf-8')
 
         req = request.Request(url, payload, headers)
         with request.urlopen(req) as request_result:
@@ -55,17 +57,17 @@ class CalcTreeClient:
 
     def _raise_exception_for_failed_request(self, request_result):
         if not (200 <= request_result.status < 300):
-            error_message = request_result.read().decode("utf-8")
-            raise Exception(f"HTTP request failed with status code {request_result.status}: {error_message}")
+            error_message = request_result.read().decode('utf-8')
+            raise Exception(f'HTTP request failed with status code {request_result.status}: {error_message}')
 
     def _prepare_body(self, ct_cells, page_id: str):
         return {
-            "pageId": page_id,
-            "ctCells": ct_cells
+            'pageId': page_id,
+            'ctCells': ct_cells
         }
 
     def _prepare_headers(self):
         return {
-            "x-api-key": self.api_key,
-            "content-type": "application/json"
+            'x-api-key': self._api_key,
+            'content-type': 'application/json'
         }
